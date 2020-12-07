5490e9fe0e7ac5de2ea2706be27bca03e917b741
justarhymes <justin@dapshow.com>
2020-12-06 22:58:05 -0800

    log cleanup

--- src/components/courses/payment/CoursePaymentBraintree.vue
+++ src/components/courses/payment/CoursePaymentBraintree.vue
@@ -1,29 +1,26 @@
 <template>
-  <div>
-    <v-braintree 
-      authorization="xxxxxxxxxxxxxxxxxxxxxx"
-      @success="onSuccess"
-      @error="onError"
-    ></v-braintree>
-  </div>
+    <div>
+        <v-braintree
+            authorization="xxxxxxxxxxxxxxxxxxxxxx"
+            @success="onSuccess"
+            @error="onError"
+        ></v-braintree>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "CoursePayment",
-  props: {
-  },
-  methods: {
-    onSuccess (payload) {
-      let nonce = payload.nonce;
-      console.log(payload)
-      console.log(nonce)
+    name: 'CoursePayment',
+    props: {},
+    methods: {
+        onSuccess(payload) {
+            let nonce = payload.nonce
+        },
+        onError(error) {
+            let message = error.message
+            console.log('ERROR ', message)
+            // Whoops, an error has occured while trying to get the nonce
+        },
     },
-    onError (error) {
-      let message = error.message;
-      console.log('ERROR ', message);
-      // Whoops, an error has occured while trying to get the nonce
-    }
-  }
-};
+}
 </script>
