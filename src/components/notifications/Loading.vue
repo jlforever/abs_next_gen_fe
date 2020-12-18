39a96b9e46a3bf35454fcc1f54076dbe6cbaf04d
Justin King <justin@dapshow.com>
2020-12-18 14:21:02 -0800

    Loading and join updates (#44)
    * loading and join updates
    
    * one more button block attr

--- src/components/notifications/Loading.vue
+++ src/components/notifications/Loading.vue
@@ -1,15 +1,26 @@
 <template>
-  <v-overlay :value="uploadingMaterials" :z-index="2000">
-    <v-progress-circular indeterminate></v-progress-circular>
-  </v-overlay>
+    <v-overlay :value="isLoading" :z-index="2000">
+        <v-progress-circular indeterminate></v-progress-circular>
+    </v-overlay>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters } from 'vuex'
 export default {
-  name: "Loading",
-  computed: {
-    ...mapGetters(["uploadingMaterials"])
-  }
-};
+    name: 'Loading',
+    computed: {
+        ...mapGetters([
+            'uploadingMaterials',
+            'loadingRegisteredCoursesFetch',
+            'loadingCardFetch',
+        ]),
+        isLoading() {
+            return (
+                this.uploadingMaterials ||
+                this.loadingRegisteredCoursesFetch ||
+                this.loadingCardFetch
+            )
+        },
+    },
+}
 </script>
